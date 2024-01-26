eb34beab1f2f87345ca32f618e195f58b9aeaa3e
WolfieKnee <david@davidwolfson.co.uk>
2024-01-26 16:58:03 +0000

    adding latest

--- XO_Game/src/Components/Board.jsx
+++ XO_Game/src/Components/Board.jsx
@@ -1,22 +1,36 @@
 import '../Board.css'
+// import Status from './Status'
+
 export default function Board({board, setBoard, currentPlayer, setCurrentPlayer}){
-    function renderBoard(event){
-        console.dir(event.target);
-        console.log(event.target.value);
-        
+    function changeCell(event){
+        const cellId = event.target.id;
+        const newCellValue = {...board}
+
+        if(board[cellId] === ''){newCellValue[cellId] = currentPlayer
+        setBoard(newCellValue)
+
+        // review status
+            
+
+
+        let nextPlayer = 'x'
+        if (currentPlayer === 'x'){
+            nextPlayer='o'
+        }
+        setCurrentPlayer(nextPlayer)}
     }
     return(<section>
         <h2>Board</h2>
         <div id='board-container'>
-        <div id='r1c1' className='board-square' onClick={(event)=>renderBoard(event)}>{board.r1c1}</div>
-        <div id='r1c2' className='board-square'></div>
-        <div id='r1c3' className='board-square'></div>
-        <div id='r2c1' className='board-square'></div>
-        <div id='r2c2' className='board-square'></div>
-        <div id='r2c3' className='board-square'></div>
-        <div id='r3c1' className='board-square'></div>
-        <div id='r3c2' className='board-square'></div>
-        <div id='r3c3' className='board-square'></div>
+        <div id='r1c1' className='board-square' onClick={(event)=>changeCell(event)}>{board.r1c1}</div>
+        <div id='r1c2' className='board-square' onClick={(event)=>changeCell(event)}>{board.r1c2}</div>
+        <div id='r1c3' className='board-square' onClick={(event)=>changeCell(event)}>{board.r1c3}</div>
+        <div id='r2c1' className='board-square' onClick={(event)=>changeCell(event)}>{board.r2c1}</div>
+        <div id='r2c2' className='board-square' onClick={(event)=>changeCell(event)}>{board.r2c2}</div>
+        <div id='r2c3' className='board-square' onClick={(event)=>changeCell(event)}>{board.r2c3}</div>
+        <div id='r3c1' className='board-square' onClick={(event)=>changeCell(event)}>{board.r3c1}</div>
+        <div id='r3c2' className='board-square' onClick={(event)=>changeCell(event)}>{board.r3c2}</div>
+        <div id='r3c3' className='board-square' onClick={(event)=>changeCell(event)}>{board.r3c3}</div>
         </div>
         </section>)
 }
--- XO_Game/src/Components/GameManager.jsx
+++ XO_Game/src/Components/GameManager.jsx
@@ -4,16 +4,22 @@ import Reset from './Reset';
 import Status from './Status';
 export default function GameManager(){
     const [board , setBoard]=useState({
-        r1c1:'',r1c2:'',r1c3:'o',
-        r2c1:'',r2c2:'x',r2c3:'',
+        r1c1:'',r1c2:'',r1c3:'',
+        r2c1:'',r2c2:'',r2c3:'',
         r3c1:'',r3c2:'',r3c3:'',
 });
     const[currentPlayer, setCurrentPlayer]=useState('o')
+    const[winStatus, setWinStatus] = useState(false) 
     return(<section>
         <h2>GameManager</h2>
         <p>Current Player : {currentPlayer}</p>
-        <Board board={board} setBoard={setBoard} currentPlayer={currentPlayer} setCurrentPlayer={setCurrentPlayer}/>
+        <Board board={board} setBoard={setBoard} 
+            currentPlayer={currentPlayer} 
+            setCurrentPlayer={setCurrentPlayer}
+            status={winStatus}
+            setStatus={setWinStatus}
+            />
         <Reset setBoard={setBoard}/>
-        <Status/>
+        <Status board={board}/>
         </section>)
 };
